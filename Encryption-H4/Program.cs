016280dba0a609feab892931501482aa30dbaf28
TGPGamez <58465104+TGPGamez@users.noreply.github.com>
2022-06-16 12:08:02 +0200

    Add project files.

---
+++ Encryption-H4/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+using System.Security.Cryptography;
+
+using (RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider())
+{
+    byte[] data = new byte[4];
+
+    for (int i = 0; i < 10; i++)
+    {
+        rng.GetBytes(data);
+        int value = BitConverter.ToInt32(data, 0);
+        Console.WriteLine(value);
+    }
+}
