6ebcbac13acfb872b65a3612950d3223d3825fb9
TGPGamez <58465104+TGPGamez@users.noreply.github.com>
2022-06-22 12:47:44 +0200

    t

---
+++ SymmetricalEncrypt/AesEncryption.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace SymmetricalEncrypt
+{
+    public class AesEncryption
+    {
+        public byte[] Encrypt(byte[] dataToEncrypt, byte[] key, byte[] iv)
+        {
+            using (AesCryptoServiceProvider aes = new AesCryptoServiceProvider())
+            {
+                aes.Mode = CipherMode.CBC;
+                aes.Padding = PaddingMode.PKCS7;
+
+                aes.Key = key;
+                aes.IV = iv;
+
+                using (MemoryStream memory = new MemoryStream())
+                {
+                    CryptoStream cryptoStream = new CryptoStream(memory, aes.CreateEncryptor(), CryptoStreamMode.Write);
+                    cryptoStream.FlushFinalBlock();
+
+                    return memory.ToArray();
+                }
+            }
+        }
+
+        public byte[] Decrypt(byte[] dataToDecrypt, byte[] key, byte[] iv)
+        {
+            using (AesCryptoServiceProvider aes = new AesCryptoServiceProvider())
+            {
+                aes.Mode = CipherMode.CBC;
+                aes.Padding = PaddingMode.PKCS7;
+
+                aes.Key = key;
+                aes.IV = iv;
+
+                using (MemoryStream memory = new MemoryStream())
+                {
+                    CryptoStream cryptoStream = new CryptoStream(memory, aes.CreateDecryptor(), CryptoStreamMode.Write);
+                    cryptoStream.FlushFinalBlock();
+
+                    return memory.ToArray();
+                }
+            }
+        }
+    }
+}
---
+++ SymmetricalEncrypt/RandomNumberGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SymmetricalEncrypt
+{
+    public static class RandomNumberGenerator
+    {
+        public static byte[] Generate(int length)
+        {
+            using (RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider())
+            {
+                byte[] data = new byte[length];
+                rng.GetBytes(data);
+
+                return data;
+            }
+        }
+    }
+}
